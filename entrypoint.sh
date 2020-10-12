9735253b188def837b65ba77d0ef5a4ab4c0ca7d
Grachev Mikhail <work@mgrachev.com>
2020-10-12 11:49:01 +0300

    Add --quiet flag (#14)
    * Add --quiet flag
    
    * Update Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 cd "$GITHUB_WORKSPACE"
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-dotenv-linter ${INPUT_DOTENV_LINTER_FLAGS} \
+dotenv-linter --quiet ${INPUT_DOTENV_LINTER_FLAGS} \
   | reviewdog -f=dotenv-linter \
     -name="${INPUT_TOOL_NAME}" \
     -reporter="${INPUT_REPORTER}" \
