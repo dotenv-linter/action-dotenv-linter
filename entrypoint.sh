d7d932d8ca5804d1516552e6a3cb2825dd6c0879
Vivek Pandey <erpandey@yahoo.com>
2020-11-18 16:34:38 +0530

    fix lint errors for shell script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-cd "$GITHUB_WORKSPACE"
+cd "$GITHUB_WORKSPACE" || exit
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-dotenv-linter --quiet ${INPUT_DOTENV_LINTER_FLAGS} \
+dotenv-linter --quiet "${INPUT_DOTENV_LINTER_FLAGS}" \
   | reviewdog -f=dotenv-linter \
     -name="${INPUT_TOOL_NAME}" \
     -reporter="${INPUT_REPORTER}" \
     -filter-mode="${INPUT_FILTER_MODE}" \
     -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
     -level="${INPUT_LEVEL}" \
-    ${INPUT_REVIEWDOG_FLAGS}
+    "${INPUT_REVIEWDOG_FLAGS}"
