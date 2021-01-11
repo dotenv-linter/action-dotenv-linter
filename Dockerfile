a1b663a43fd40e90a4b0ffc00af36290ccc0a495
Artem Medvedev <ddttkey@gmail.com>
2021-01-11 13:51:56 +0300

    Add --no-color flag (#21) (#22)
    * Add --no-color flag (#21)
    
    * Fix an error with passing arguments
    
    * Disable shellcheck rule
    
    Co-authored-by: Grachev Mikhail <work@mgrachev.com>

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:3.11
 
-ENV DOTENV_LINTER_VERSION v2.2.1
+ENV DOTENV_LINTER_VERSION v3.0.0
 ENV REVIEWDOG_VERSION v0.11.0
 
 SHELL ["/bin/ash", "-eo", "pipefail", "-c"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+# shellcheck disable=SC2086
+
 cd "$GITHUB_WORKSPACE" || exit
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-dotenv-linter --quiet "${INPUT_DOTENV_LINTER_FLAGS}" \
+dotenv-linter --quiet --no-color ${INPUT_DOTENV_LINTER_FLAGS} \
   | reviewdog -f=dotenv-linter \
     -name="${INPUT_TOOL_NAME}" \
     -reporter="${INPUT_REPORTER}" \
     -filter-mode="${INPUT_FILTER_MODE}" \
     -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
     -level="${INPUT_LEVEL}" \
-    "${INPUT_REVIEWDOG_FLAGS}"
+    ${INPUT_REVIEWDOG_FLAGS}
